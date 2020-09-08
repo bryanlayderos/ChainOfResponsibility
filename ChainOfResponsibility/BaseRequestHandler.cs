f62a8373a49aa51b0f4197fcc336ee1b860c4118
bryanlayderos <70880321+bryanlayderos@users.noreply.github.com>
2020-09-08 17:07:43 +0800

    Add files via upload

---
+++ ChainOfResponsibility/BaseRequestHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace ChainOfResponsibility
+{
+    public interface IBaseRequestHandler
+    {
+        void SetNextChain(IBaseRequestHandler handler);
+
+        void Deserialize(Request request);
+    }
+}
---
+++ ChainOfResponsibility/JSONRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace ChainOfResponsibility
+{
+    using System;
+
+    public class JSONRequestHandler : IBaseRequestHandler
+    {
+        private readonly string contentType = "JSON";
+
+        private IBaseRequestHandler nextChain;
+
+        public void SetNextChain(IBaseRequestHandler handler)
+        {
+            this.nextChain = handler;
+        }
+
+        public void Deserialize(Request request)
+        {
+            if (request.ContentType.Equals(this.contentType))
+            {
+                Console.WriteLine("Content-type: " + request.ContentType);
+                Console.WriteLine("Content: " + request.Content + "\n");
+            }
+            else
+            {
+                Console.WriteLine("The content type of this request is not valid." + "\n");
+            }
+        }
+    }
+}
