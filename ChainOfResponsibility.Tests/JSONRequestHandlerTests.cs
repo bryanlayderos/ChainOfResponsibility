d49546acd7260195f13053f8fe5983fb7856ff25
bryanlayderos <70880321+bryanlayderos@users.noreply.github.com>
2020-09-10 09:46:18 +0800

    Add files via upload

---
+++ ChainOfResponsibility.Tests/JSONRequestHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ChainOfResponsibility.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class JSONRequestHandlerTests
+    {
+        private readonly Mock<ILogger> debugLoggerMock = new Mock<ILogger>();
+
+        [TestMethod]
+        public void Deserialize_IfContentTypeIsJSON_LogContentType()
+        {
+            // Arrange
+            string expectedOutput = "Content-type: JSON";
+            Request request = new Request("JSON", "This is an JSON content");
+            IBaseRequestHandler handlerRequest = new JSONRequestHandler();
+
+            // Act
+            handlerRequest.Deserialize(request, debugLoggerMock.Object);
+
+            // Assert
+            debugLoggerMock.Verify(x => x.Log(expectedOutput));
+        }
+    }
+}
